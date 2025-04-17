5440cd10220cbc3e4b78d81ded90c7014d3639b5
ViosSixth <risimac@outlook.com>
2025-04-17 21:04:28 +0200

    week 4 assignment

--- WK4-Assignment.py
+++ WK4-Assignment.py
@@ -1,2 +1,18 @@
-open("wishlist.docx","r")
-with open("Instructions.txt","w") as file: file.write("Avuxeni")
+def file_handling_function():
+    filename=input("Instructions.txt")
+
+try:
+    with open("Instructions.txt","r") as file:
+        data=file.read()
+
+    modified_content= file.upper()
+
+    new_file = f"modified_{'New-Instructions.txt'}"
+
+    with open("New-Instructions.txt","w") as file:
+        new_file.write("modified")
+
+    print (f"modified Instructions to {'New-Instructions'}")
+    
+except FileNotFoundError:
+    print("File not found.")
